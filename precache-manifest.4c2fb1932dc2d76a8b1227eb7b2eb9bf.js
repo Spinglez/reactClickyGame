4ce77f2ebb7beded782269e00604c990c0e133b1
Spinglez <42229372+Spinglez@users.noreply.github.com>
2019-02-13 18:15:10 -0800

    Updates

---
+++ precache-manifest.4c2fb1932dc2d76a8b1227eb7b2eb9bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2db37c3c3c4825541f41",
+    "url": "/reactClickyGame/static/js/runtime~main.2db37c3c.js"
+  },
+  {
+    "revision": "02b5b9a44a007e9f1540",
+    "url": "/reactClickyGame/static/js/main.02b5b9a4.chunk.js"
+  },
+  {
+    "revision": "8e38578387f616a60c45",
+    "url": "/reactClickyGame/static/js/1.8e385783.chunk.js"
+  },
+  {
+    "revision": "02b5b9a44a007e9f1540",
+    "url": "/reactClickyGame/static/css/main.0e9a1b9c.chunk.css"
+  },
+  {
+    "revision": "7167fe74c41ab6de1acf096b56f0fd98",
+    "url": "/reactClickyGame/index.html"
+  }
+];
